3b85e10a91c8280344df8a29937fb406c3eb4cca
nphoton <nphoton@NIST.GOV>
2018-08-10 16:17:57 -0400

    Update keysight laser

--- pyLaser/keysight.py
+++ pyLaser/keysight.py
@@ -2,7 +2,7 @@ import visa
 import numpy as np
 import time
 
-def Keysight8164B():
+class Keysight8164B():
 
     def __init__(self, **kwargs):
         self.address = kwargs.get('address', "GPIB0::20::INSTR")
@@ -10,50 +10,91 @@ def Keysight8164B():
         self._power = None
         self._attenuation = None
         self._reset = None
+        self._connected = False
+
+    def _isOpen(fun):
+        def wrapper(*args, **kwargs):
+            self_app = args[0]
+            if self_app._connected:
+                out = fun(*args, **kwargs)
+                return out
+        return wrapper 
 
     @property
+    def connected(self):
+        return self._connected
+
+    @connected.setter
+    def connected(self, val):
+        if val: 
+            rm = visa.ResourceManager()
+            if self.address in rm.list_resources():
+                self._instr = rm.open_resource(self.address)
+                self._connected = True
+                word = 'OUTP:POW:UNIT DBM'
+                self._instr.write(word)
+        else:
+            self._inst.close()
+            self._connected = False
+
+    @property
+    @_isOpen
     def lbd(self):
-        lbd = self.instr.querry('WAV?')
-        return lbd*1e9
+        self._lbd = self._instr.query('WAV?')
+        self._lbd = float(self._lbd.strip())
+        return self._lbd*1e9
 
-    ~@lbd.setter
+
+    @lbd.setter
+    @_isOpen
     def lbd(self, val):
-        self._lbd = 'WAV {:3.f}E-9'.format(val)
-        self.instr.write(self._lbd)
+        word = 'WAV {:.3f}E-9'.format(val)
+        self._instr.write(word)
 
     @property
+    @_isOpen
     def power(self):
-        self._power = self.instr.querry('POW?')
+        self._power = self._instr.query('POW?')
+        self._power = float(self._power.strip())
         return self._power
 
     @power.setter
+    @_isOpen
     def power(self, val):
-        word = "POW {:.3f}dBm".format(val)
-        self.instr.write(word)
+        word = "POW {:.3f} DBM".format(val)
+        self._instr.write(word)
 
     @property
+    @_isOpen
     def reset(self):
         return self._reset
     
     @reset.setter
+    @_isOpen
     def reset(self, val):
         if val:
             word = "*RST"
-            self.instr.write(word)
+            self._instr.write(word)
             time.sleep(0.5)
             # Changing unit to dBm
-            word = 'pow:unit DBM'
-            self.instr.write(word)
+            word = 'OUTP:POW:UNIT DBM'
+            self._instr.write(word)
 
     @property
+    @_isOpen
     def attenuation(self):
-        self._attenuation = self.instr.querry('INP:ATT?')
+        self._attenuation = self._instr.query('INP:ATT?')
+        self._attenuation = float(self._attenuation.strip())
         return self._attenuation
     
     @attenuation.setter
+    @_isOpen
     def attenuation(self,val):
         word = "INP:ATT {:.3f}".format(val)
-        self.instr.write(word)
+        self._instr.write(word)
+
+if __name__ == "__main__":
+    lsr = Keysight8164B()
 
 
 

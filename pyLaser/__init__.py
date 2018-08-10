5bca42d45af356234b96f1426dceaa7020805f87
gregmoille <greg.moille@gmail.com>
2018-08-10 11:41:30 -0400

    Add Keysight Laser

--- pyLaser/__init__.py
+++ pyLaser/__init__.py
@@ -1,2 +1,3 @@
 from .newfocus import NewFocus6700
 from .toptica import Toptica1050
+from .keysightlaser import KeysightLaser
---
+++ pyLaser/keysightlaser.py
@@ -0,0 +1,60 @@
+*RST
+*INP:ATT
+WAV
+POW
+
+import visa
+import numpy as np
+
+def KeysightLaser():
+
+    def __init__(self, **kwargs):
+        self.address = kwargs.get('address', None)
+        self._lbd = None
+        self._power = None
+        self._attenuation = None
+        self._reset = None
+
+    @property
+    def lbd(self):
+        lbd = self.instr.querry('WAV?')
+        return lbd*1e9
+
+    ~@lbd.setter
+    def lbd(self, val):
+        self._lbd = 'WAV {:3.f}E-9'.format(val)
+        self.instr.write(self._lbd)
+
+    @property
+    def power(self):
+        self._power = self.instr.querry('POW?')
+        return self._power
+
+    @power.setter
+    def power(self, val):
+        word = "POW {:.3f}dBm".format(val)
+        self.instr.write(word)
+
+    @property
+    def reset(self):
+        return self._reset
+    
+    @reset.setter
+    def reset(self, val):
+        if val:
+            word = "*RST"
+            self.instr.write(word)
+            # need to change everything in dBm
+
+    @property
+    def attenuation(self):
+        self._attenuation = self.instr.querry('INP:ATT?')
+        return self._attenuation
+    
+    @attenuation.setter
+    def attenuation(self,val):
+        word = "INP:ATT {:.3f}".format(val)
+        self.instr.write(word)
+
+
+
